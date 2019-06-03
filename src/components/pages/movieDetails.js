bd76912e23e9bd785a3ded1e640796ef44a5dc70
Orit Kozolin <orit@vcita.com>
2019-06-03 18:47:24 +0300

    half way refactoring before adjusting route for details page

--- src/components/pages/movieDetails.js
+++ src/components/pages/movieDetails.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from "../shared/Navigation";
+import ShowItem from "../shared/ShowItem";
 
-const  MovieDetails = () => {
+const  MovieDetails = (props) => {
     return (
         <div>
         <h1>Movie details</h1>
         <Navigation/>
+
+        {/*<ShowItem url="https://via.placeholder.com/600/92c952" id={props.id}></ShowItem>*/}
+        <ShowItem url={props.url} id={props.id}></ShowItem>
         </div>
     );
 }
