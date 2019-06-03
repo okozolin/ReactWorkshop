4dc2c4e31387610548b2f6c6b27d1a7417ff9024
Orit Kozolin <orit@vcita.com>
2019-06-03 15:07:16 +0300

    read from server the photos json with api and run the call only once using 'useEffect'

--- src/components/pages/MovieIndex.js
+++ src/components/pages/MovieIndex.js
@@ -1,21 +1,30 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Navigation from "../shared/Navigation";
 import {apiRequest} from "../../services/api";
 
 const MovieIndex = (props) => {
-    const [movies, setMovies ]= useState([])
+    const [movies, setMovies ]= useState([]);
 
-    apiRequest().then( data => setMovies(data))
+    // the code will run once when the function is run (mounted) for first time
+    useEffect(() => {
+        // once
+
+        apiRequest().then( data => setMovies(data));
+
+        // unmount when element dead print the message
+        return () => console.log("I'm dead");
+        }, []);
+
     return (
         <div>
             <h1>Movies index</h1>
+            <Navigation/>
             { movies.map( movie => (
                 <pre>
                     {JSON.stringify(movie, 1, 1)}
                 </pre>
                 )
             ) }
-            <Navigation/>
         </div>
     );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,6 @@ const URL = "http://jsonplaceholder.typicode.com/photos";
 
 export function apiRequest () {
     // bring the data from server side using browser 'fetch' method api
-    fetch(URL).then( response => response.json())
+    return fetch(URL).then( response => response.json())
 
 }
